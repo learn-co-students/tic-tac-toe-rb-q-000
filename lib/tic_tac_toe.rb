b09992c769ac93f472d21f137ede2f66a9a37f26
Saturn226 <serenehopes@gmail.com>
2015-10-16 23:13:24 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,42 +4,45 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-  ]
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
- def won?(board)
-  WIN_COMBINATIONS.detect  do | combination| 
+def won?(board)
+ WIN_COMBINATIONS.detect  do | combination| 
     position_taken?(board,combination[0]) && (board[combination[0]] == board[combination[1]] && board[combination[2]] == board[combination[1]])
+
   end
   
- end
+end
+
+=begin def full?(board)
+ board.each_index do |position| 
+  if !position_taken?(board,position)
+    false
+    break
+  else
+    true
+  end
+end
+
+end
+=end
 
 def full?(board)
-     board.each_index do |position| 
-      if !position_taken?(board,position)
-        false
-        break
-      else
-        true
-      end
-    end
-    
+   board.all?{|pos| pos == "X" || pos == "O"}
 end
 
 def draw?(board)
-  if full?(board) && !won?(board)
-    true
-  else
-    false
-  end
+  full?(board) && !won?(board)
+ 
 end
 
 
@@ -64,13 +67,7 @@ def winner(board)
 end
 
 def turn_count(board)
-  count = 0
-  board.each do |place|
-    if place == "X" or place == "O"
-      count+= 1
-    end
-  end
-  count
+  board.count{|t| t == "X" || t == "O"}
 end
 
 def current_player(board)
