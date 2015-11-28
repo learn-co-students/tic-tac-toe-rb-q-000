9b0a2a9051b93e01835851ebc54761b4cf719b88
ryuichi7 <brettrheenan@gmail.com>
2015-11-28 10:19:13 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,84 @@
+WIN_COMBINATIONS = [
+  [0,1,2],[3,4,5],
+  [6,7,8],[0,3,6],
+  [1,4,7],[2,5,8],
+  [0,4,8],[6,4,2]]
+
+  def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, position)
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if valid_move?(board, input)
+    move(board, input)
+  else
+    turn(board)
+  end
+  display_board(board)
+end
+
+def play(board)
+  times = 0
+  until times == 9
+  times += 1
+  turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |number|
+    if number == "X" || number == "O"
+       counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
