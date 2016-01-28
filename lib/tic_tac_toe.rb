ddf8a93496d0cd78bc53a67876805488694b1d2f
Connor Marble <connor.marble@gmail.com>
2016-01-28 02:33:43 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,13 +9,14 @@
     [6,4,2]
   ]
 
-  def display_board(board)
-    puts   " #{board[0]} ""|"" #{board[1]} ""|"" #{board[2]} "
-    puts              "-----------"
-    puts   " #{board[3]} ""|"" #{board[4]} ""|"" #{board[5]} "
-    puts              "-----------"
-    puts   " #{board[6]} ""|"" #{board[7]} ""|"" #{board[8]} "
-  end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 
   def move(board, location, player = 'X')
     board[location.to_i-1] = player
@@ -49,7 +50,7 @@
   end
 
 
- 
+
 
   def current_player(board)
      turn_count(board) % 2 == 0 ? "X" : "O"
