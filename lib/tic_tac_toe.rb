285faed2dadddb169dc7679e4de65dbff585ce4c
Connor Marble <connor.marble@gmail.com>
2016-01-28 02:36:08 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,7 @@ end
 
 
   def move(board, location, player = 'X')
-    board[location.to_i-1] = player
+    board[location.to_i - 1] = player
   end
 
   def position_taken?(board, location)
@@ -37,7 +37,7 @@ end
 
   def turn(board)
     puts "Please enter 1-9:"
-    input = gets.strip
+    input = gets
     if !valid_move?(board, input)
         turn(board)
     end
@@ -52,31 +52,39 @@ end
 
 
 
-  def current_player(board)
-     turn_count(board) % 2 == 0 ? "X" : "O"
+def current_player(board)
+
+  if turn_count(board) % 2 == 0
+
+    "X"
+
+  else
+
+  "O"
   end
+end
 
-  def won?(board)
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index = win_combination
+          position_1 = board[win_index[0]]
+          position_2 = board[win_index[1]]
+          position_3 = board[win_index[2]]
 
-    WIN_COMBINATIONS.each do | win_combination |
 
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return win_combination
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return win_combination
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination
+      elsif   position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return win_combination
       end
-    end
-    false
-  end
 
+  end
+      if board ==  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      false
+    end
+
+end
 
 
   def full?(board)
