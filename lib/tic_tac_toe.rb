d78ec99a56a52274e08c05257ebf8b837b54f125
jbok4 <jbok4@aol.com>
2016-02-11 20:05:09 -0500

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,12 +10,12 @@ WIN_COMBINATIONS = [
  board = [" "," "," "," "," "," "," "," "," "]
  
  def display_board(board)
-  print " #{board[0]} | #{board[1]} | #{board[2]} "
-  print "-----------"
-  print " #{board[3]} | #{board[4]} | #{board[5]} "
-  print "-----------"
-  print " #{board[6]} | #{board[7]} | #{board[8]} "
- end
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
 def move(board, position, player = "X")
   board[position.to_i-1] = player
