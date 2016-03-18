738a55fe830997a4f8975ba782160d3869afef7f
JennyShalai <jenny.shalai@gmail.com>
2016-03-18 14:17:01 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -132,11 +132,11 @@ end
 def play(board)
   while !over?(board)
     turn(board)
-    if won?(board)
-      puts "Congratulations #{winner(board)}!"
-    elsif draw?(board)
-      puts "Cats Game!"
-      return
-    end
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+    return
   end
 end
