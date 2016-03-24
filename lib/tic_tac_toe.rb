48d06b311b1ffdc40a6aaa429b86af78a929fc91
MichaelChen1120 <michael.chen.1120@gmail.com>
2016-03-24 13:25:27 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,19 +18,15 @@ def move(board,position,char="X")
 end
 
 def position_taken?(board,position)
-  if board[position.to_i-1] == " "
+  if board[position.to_i] == " "
     false
-    elsif board[position.to_i-1] == ""
-    false
-    elsif board[position.to_i-1] == nil
-    false
-    else board[position.to_i-1] == "X" || "O"
+    else board[position.to_i] == "X" || "O"
     true
   end
 end
 
 def valid_move?(board,position)
- if position.to_i.between?(1,9)  && !position_taken?(board,position)
+ if position.to_i.between?(1,9)  && !position_taken?(board,position.to_i-1)
    true
  end
 end
