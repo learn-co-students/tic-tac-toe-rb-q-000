def509cb5e0ed86a8678be76ce1ca7ff2501c569
scottibe <bewick.scott@gmail.com>
2016-04-12 13:06:29 -0500

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,7 @@ end
 
 def position_taken?(board, position)
 if board[position] == " " || board[position] == "" || board[position] == nil
-  false
+  false 
 else board[position] == "X" || board[position] == "O"
 true
   end
