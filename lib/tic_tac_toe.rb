9a623ee9024b2860b7c882543bf397dabbef9a0e
nevidnichiy <nevidnichiy@gmail.com>
2016-04-19 19:32:14 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,15 +20,6 @@ def play(board)
   end
 end
 
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
 def move(board, pos, char = "X")
   board[pos.to_i - 1] = char
 end
@@ -45,20 +36,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def position_taken?(board, position)
-  if board[position] == "X" || board[position] == "O"
-    true
-  else
-    false
-  end
-end
-
 def valid_move?(board, position)
   position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
-end
-
-def move(board, pos, sign = "X")
-  board[pos.to_i - 1] = sign
 end
 
 def turn(board)
