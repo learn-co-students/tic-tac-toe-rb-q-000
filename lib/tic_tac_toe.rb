e8627c993fd9f44d2d5c4d1db00540e95b9ef0c4
avanover0311 <avanover0311@gmail.com>
2016-04-20 03:37:23 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,5 @@
 WIN_COMBINATIONS =[ [0,1,2], [3,4,5], [6,7,8], [0,4,8], [1,4,7], [0,3,6], [2,5,8], [6,4,2] ]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -9,9 +10,6 @@ end
 
 def move(board, position, value = "X")
   board[position.to_i - 1] = value
-
-
-  return board
 end
 
 def position_taken?(board, location)
@@ -21,7 +19,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   position = gets.strip
-  if valid_move?(board, position) == true
+  if valid_move?(board, position)
    move(board, position, current_player(board))
   else
     turn(board)
@@ -30,87 +28,64 @@ def turn(board)
 end
 
 def turn_count(board)
-counter = 0
+  counter = 0
   board.each do |index|
-if index == "X" || index == "O"
-  counter += 1
-end
-
+    if index == "X" || index == "O"
+      counter += 1
+    end
   end
-counter
+  counter
 end
-
 
 def current_player(board)
-  if turn_count(board) %2 == 0
-    "X"
-    else "O"
+  if turn_count(board) % 2 == 0
+    "X" 
+  else
+    "O" 
   end
-
 end
 
+
 def won?(board)
- WIN_COMBINATIONS.each do |combos|
-   if board[combos[0]] == " " && board[combos[1]] == " " && board[combos[2]] == " "
-  false
-     elsif board[combos[0]] == board[combos[1]] && board[combos[0]] == board[combos[2]]
-       return combos
-     end
-   end
-  false
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
 end
 
 def full?(board)
   board.each do |board_position|
     if board_position == " "
       return false
-  end
+    end
   end
 end
 
 
 def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  else
-    return false
- end
+  !won?(board) && full?(board)
 end
 
  def over?(board)
-  if won?(board) || full?(board)
-  return true
-  else
-  return false
-
-  end
+  won?(board) || draw?(board)
  end
 
-def valid_move?(board, position)
-  position = position.to_i - 1
-  if position_taken?(board, position) == false && position.between?(0, 10)
-    return true
-  elsif board[position] == "X" || board[position] == "O"
-    return false
-  end
+    def valid_move?(board, position)
+      input.to_i.between?(1,9) && !position_taken?(board, input.to_i-1)
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-  def play(board)
+def play(board)
   while !over?(board)
     turn(board)
   end
-    current_player(board)
   if won?(board)
-    puts "Player somebody won"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
-    puts "This game is a draw"
+    puts "Cats Game!"
   end
-  end
-
-
-
+end
 
 def winner(board)
   if winning_combo = won?(board)
@@ -118,6 +93,3 @@ def winner(board)
   end
 end
 
-
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
