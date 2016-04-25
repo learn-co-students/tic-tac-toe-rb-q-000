f1a3ef6c4fb3f3ae9729064271b08e8c91de7f17
m-maste <mitchell.masterson@gmail.com>
2016-04-25 00:22:39 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,26 @@
-# Helper Method
-def position_taken?(board, location)
-  !(board[location].nil? || board[location] == " ")
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
 end
 
-def display_board(board = [" "," "," "," "," "," "," "," "," "])
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -11,81 +28,58 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
-
-def draw?(board)
-  full?(board) && !won?(board)
-end
-
-def full?(board)
-  board.all? {|position| position == "X" || position == "O"}
-end
-
-def over?(board)
-  draw?(board) || won?(board).kind_of?(Array)
+def valid_move?(board, input)
+  input.to_i.between?(1,9) && !position_taken?(board, input.to_i-1)
 end
 
 def won?(board)
-#  winning_combo = []
-#  win = true
-#  WIN_COMBINATIONS.each do |combo|
-#    combo.all? {|position| board[position] == "X"} ? winning_combo = combo : win = false
-#    combo.all? {|position| board[position] == "O"} ? winning_combo = combo : win = false
-#  end
-#  !winning_combo.empty? ? winning_combo : win
-
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
     board[combo[1]] == board[combo[2]] &&
     position_taken?(board, combo[0])
   end
-
 end
 
-def winner(board)
-  if won?(board)
-    winning_combo = won?(board)
-    board[winning_combo[0]]
-  else
-    nil
-  end
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
 end
 
-def move(board, position, token = "X")
-  board[position.to_i - 1] = token
+def draw?(board)
+  !won?(board) && full?(board)
 end
 
-def valid_move?(board, position)
-  position.to_i.between?(1,9) && !position_taken?(board, position.to_i - 1)
+def over?(board)
+  won?(board) || draw?(board)
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  position = gets.chomp
-  if !valid_move?(board, position)
+  input = gets.strip
+  if !valid_move?(board, input)
     turn(board)
-  else
-    move(board, position)
-    display_board(board)
   end
+  move(board, input, current_player(board))
+  display_board(board)
 end
 
-def turn_count(board)
-  i = 0
-  board.each {|position| position == "X" || position == "O" ? i += 1 : i}
-  i
+def position_taken?(board, location)
+  !(board[location].nil? || board[location] == " ")
 end
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+
+def move(board, location, player = "X")
+  board[location.to_i-1] = player
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
 end
