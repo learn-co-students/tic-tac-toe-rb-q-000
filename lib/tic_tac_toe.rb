bffbec36f2e70a19a8f9cd943a549b56405fe471
Kristi Thompson <kristiforpresident@gmail.com>
2016-04-28 18:06:13 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,108 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, position, token)
+  index = position.to_i - 1
+  board[index] = token
+end
+
+def position_taken?(board, position)
+  !(board[position] == " " || board[position] == "" || board[position] == nil)
+end
+
+def valid_move?(board, position)
+  index = position.to_i - 1
+
+  if index > 8 || index < 0
+    false
+  else
+    !position_taken?(board, index)
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+
+  if valid_move?(board, input)
+    move(board, input, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  count = board.select {|i| i != " "}
+  count.length
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+end
+
+def play(board)
+
+  until over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
+
+
+def won?(board)
+
+  WIN_COMBINATIONS.detect do |win_combination|
+      if win_combination.all? {|i| board[i] == "X"} || win_combination.all? {|i| board[i] == "O"}
+        win_combination
+      else
+        false
+      end
+  end
+
+end
+
+def full?(board)
+  !(board.include?(" ") || board.include?(nil))
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  if won?(board)
+    board[won?(board)[0]]
+  else
+    nil
+  end
+end
