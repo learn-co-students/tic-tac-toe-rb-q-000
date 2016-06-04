c7390e659467d5c99bd6da8d54c2696a1f3b7a06
P Joshi <joshipv2@gmail.com>
2016-06-03 22:17:51 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,43 +24,33 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
+def play(board)
 
-def move(board, index, current_player)
-  board[index] = current_player
-end
+    until over?(board)
+      turn(board)
+    end
+    if won?(board)
+        won = won?(board)
+        #require 'pry'
+        #binding.pry
+        puts "Congratulations #{board[won[0]]}!"
+        #binding.pry
+    elsif draw?(board)
+        puts "Cats Game!"
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+    end
 end
 
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-
   if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
   end
-end
-
-# Define your play method below
-
-def play(board)
-    count = 0
-    while count < 9
-      turn(board)
-      count += 1
-    end
 end
 
 def current_player(board)
@@ -82,6 +72,21 @@ def turn_count(board)
   return counter
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+#def position_taken?(board, location)
+#  board[location] != " " && board[location] != ""
+#end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
 def full?(board)
   !board.include? " "
