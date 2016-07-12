de664af804422a562cd9de7335136347ce9c63dc
Kenlyn Terai <kkterai@gmail.com>
2016-07-12 06:34:16 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,27 +23,16 @@ def input_to_index (input)
   index = input.to_i - 1
 end
 
-def move(board,index,value)
-  board[index]=value
+def move (board,index,value)
+  board[index] = value
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+def position_taken?(board, index)
+  board[index] != " " && board[index] != ""
 end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board,index)
-    move(board,index,value)
-    display_board(board)
-  else
-  end
 end
 
 def turn_count(board)
@@ -52,13 +41,28 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board).even? ? "O" : "X"
+  turn_count(board).even? ? "X" : "O"
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    value = current_player(board)
+    move(board,index,value)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
 
 def won?(board)
   WIN_COMBINATIONS.find do |combo|
-    board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" ||
-    board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" ||
+      board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+      true
+  end
   end
 end
 
@@ -73,26 +77,30 @@ def draw?(board)
 end
 
 def over?(board)
-  won?(board) || full?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end
 end
 
 def winner(board)
   WIN_COMBINATIONS.find do |combo|
     if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
-    return "X"
-    elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
-    return "O"
+      return "X"
+    elsif  board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+      return "O"
     end
   end
 end
 
 def play(board)
   until over?(board)
-  turn(board)
-  if won?(board) == true
-    "Congratulations #{won?(board)}!"
-  elsif draw?(board) == true
-    "Cats Game!"
+    turn(board)
   end
-end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
 end
